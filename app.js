e88f7738f6d3c8c7f9d9a6816c4d19e8aa269a3b
Karen Allones <karenallones03@gmail.com>
2026-04-05 12:13:25 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // change if you use pug or hbs
